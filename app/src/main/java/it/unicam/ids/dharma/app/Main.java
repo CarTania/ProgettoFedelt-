703eb1a9aad160b7b79a55ef20d52a3f61c5227b
Tania <taniacart.1991@gmail.com>
2023-02-07 22:39:24 +0100

    Aggiunto ApriMenuRicerca.

---
+++ app/src/main/java/it/unicam/ids/dharma/app/Main.java
@@ -0,0 +1,13 @@
+package it.unicam.ids.dharma.app;
+
+public class Main {
+
+    public static void main (String [] args)
+    {
+        AreaRiservataCliente a= new AreaRiservataCliente();
+
+        a.apriMenuRicerca();
+
+        System.out.println ("Inserisci il programma fedeltà: ");
+    }
+}
