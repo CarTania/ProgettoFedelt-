446aae5199c36d847053544de28cf302a76bc0f3
Tania <taniacart.1991@gmail.com>
2023-02-11 19:38:01 +0100

    Aggiunta ed implementata la classe ServizioPremium

---
+++ app/src/main/java/it/unicam/ids/dharma/app/ServizioPremium.java
@@ -0,0 +1,28 @@
+package it.unicam.ids.dharma.app;
+
+public class ServizioPremium {
+
+    private String nome;
+    private String tipologia;
+
+    public ServizioPremium(String nome, String tipologia) {
+        this.nome = nome;
+        this.tipologia = tipologia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipologia() {
+        return tipologia;
+    }
+
+    public void setTipologia(String tipologia) {
+        this.tipologia = tipologia;
+    }
+}
