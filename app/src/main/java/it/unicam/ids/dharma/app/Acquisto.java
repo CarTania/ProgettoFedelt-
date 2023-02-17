ee0ee2174f65c0279c9c97f59ae02c93609c8449
Tania <tania.cartechini@studenti.unicam.it>
2023-02-17 19:50:14 +0100

    Aggiunta la classe Acquisto

---
+++ app/src/main/java/it/unicam/ids/dharma/app/Acquisto.java
@@ -0,0 +1,56 @@
+package it.unicam.ids.dharma.app;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Acquisto {
+
+    private final LocalDate dataAcquisto;
+
+    private List<Prodotto> listaProdotti;
+
+    private final Cliente cliente;
+
+    public Acquisto(LocalDate dataAcquisto, Cliente cliente) {
+        this.dataAcquisto = dataAcquisto;
+        this.cliente = cliente;
+        this.listaProdotti= null;
+    }
+
+    public boolean aggiungiProdotto(Prodotto prodotto)
+    {
+        return this.listaProdotti.add(prodotto);
+
+    }
+
+    public boolean rimuoviProdotto(Prodotto prodotto)
+    {
+        return this.listaProdotti.remove(prodotto);
+    }
+
+
+    public double totaleAcquisto()
+    {
+        double sum= 0;
+
+        for (Prodotto p: listaProdotti)
+        {
+            sum+= p.getCosto();
+        }
+        return sum;
+    }
+
+
+    public LocalDate getDataAcquisto() {
+        return dataAcquisto;
+    }
+
+    public List<Prodotto> getListaProdotti() {
+        return listaProdotti;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+}
