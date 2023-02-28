e4172cf1a906cc563e7e6394611793e0862b6a5d
Luca Malvatani <luca.malvatani@studenti.unicam.it>
2023-02-28 23:57:36 +0100

    aggiunta classe Acquisto e interfaccia ITitolare

---
+++ app/src/main/java/it/unicam/ids/dharma/app/ITitolare.java
@@ -0,0 +1,17 @@
+package it.unicam.ids.dharma.app;
+
+/**
+ * Interfaccia ITitolare
+ */
+
+public interface ITitolare {
+    boolean attivaProgramma (int idProgramma);
+    boolean disattivaProgramma(int idProgramma);
+    void visualizzaStatistiche();
+    int creaCatalogo();
+    boolean modificaCatalogo(int idCatalogo);
+    int definisciServizioPremium();
+    boolean modificaServizioPremium(int idServizio);
+    boolean rimuoviServizioPremium(int idServizio);
+    boolean rimuoviCatalogo(int idCatalogo);
+}
