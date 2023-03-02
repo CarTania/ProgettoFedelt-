3ceea5b507e4edca4929533db60ac5454741110a
Antonio Addesa <antonio.addesa@studenti.unicam.it>
2023-03-02 16:18:31 +0100

    Aggiornata la classe Catalogo

--- app/src/main/java/it/unicam/ids/dharma/app/Catalogo.java
+++ app/src/main/java/it/unicam/ids/dharma/app/Catalogo.java
@@ -1,41 +1,37 @@
 package it.unicam.ids.dharma.app;
 
-import org.apache.commons.lang3.tuple.MutablePair;
-import org.apache.commons.lang3.tuple.Pair;
-
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class Catalogo {
-
     private final int id;
-    private final Map<Prodotto, Pair<Integer, Integer>> listapremi;
+    private final Map<Prodotto, AttributiPremio> listapremi;
 
     public Catalogo(int id) {
-        this.id= id;
+        this.id = id;
         this.listapremi = new HashMap<>();
     }
-
     public int getId() {
         return id;
     }
-
-    public Map<Prodotto, Pair<Integer, Integer>> getListapremi() {
+    public Map<Prodotto, AttributiPremio> getListapremi() {
         return listapremi;
     }
-
-    public boolean aggiungiAlCatalogo(Prodotto prodotto, int quantita, int punti)
+    public boolean inserisciPremio(Prodotto prodotto, int quantita, int punti)
     {
         if(!(listapremi.containsKey(prodotto)))
         {
-            listapremi.put(prodotto, new MutablePair<>(quantita, punti));
-            return true;
+            //se c'è una quantità sufficiente di quel prodotto, lo inserisco nel catalogo
+            if(prodotto.getQuantita() >= quantita){
+                listapremi.put(prodotto, new AttributiPremio(quantita, punti));
+                prodotto.setQuantita(prodotto.getQuantita() - quantita);
+                return true;
+            }
         }
         return false;
     }
-
-    public boolean eliminaDalCatalogo(Prodotto prodotto)
+    public boolean rimuoviPremio(Prodotto prodotto)
     {
         if (listapremi.containsKey(prodotto))
         {
@@ -43,6 +39,72 @@ public class Catalogo {
             return true;
         }
         return false;
+    }
+
+    //assegna un numero di punti diverso a uno specifico premio.
+    public boolean aggiornaPuntiPremio(Prodotto prodotto, int punti){
+        if(listapremi.containsKey(prodotto)){
+            listapremi.get(prodotto).setPunti(punti);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean aggiornaQuantitaPremio(Prodotto prodotto, int quantita){
+        if(listapremi.containsKey(prodotto)){
+            if(prodotto.getQuantita() >= quantita){
+                listapremi.get(prodotto).setQuantita(quantita);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*
+        * questo metodo viene chiamato quando un premio viene riscosso da un cliente.
+        * La quantità dello specifico premio viene decrementata di un' unità e il prodotto
+        *  viene restituito (se ci sono ancora unità disponibili).
+     */
+    public Optional<Prodotto> emettiPremio(Prodotto prodotto) throws CloneNotSupportedException {
+        if(listapremi.containsKey(prodotto)){
+            //se il premio è disponibile nel catalogo
+            if(listapremi.get(prodotto).getQuantita() != 0){
+                aggiornaQuantitaPremio(prodotto,
+                    listapremi.get(prodotto).getQuantita() - 1);
+                return Optional.ofNullable(prodotto.ottieniProdotto());
+            }
+        }
+        return null;
+    }
+
+    /*
+        * classe annidata che rappresenta gli attributi di un premio (quantità e punti
+        * associati al premio) presente nel catalogo.
+     */
+    class AttributiPremio{
+        private int quantita;
+        private int punti;
+
+        public AttributiPremio(int quantita, int punti) {
+            this.quantita = quantita;
+            this.punti = punti;
+        }
+
+        public int getQuantita() {
+            return quantita;
+        }
+
+        public void setQuantita(int quantita) {
+            this.quantita = quantita;
+        }
+
+        public int getPunti() {
+            return punti;
+        }
+
+        public void setPunti(int punti) {
+            this.punti = punti;
+        }
     }
 }
 
