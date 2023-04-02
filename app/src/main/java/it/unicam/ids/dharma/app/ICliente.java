1f15343c393168fcbf35f28e6ad9448b8eba6f68
Antonio Addesa <antonio.addesa@studenti.unicam.it>
2023-04-02 19:22:48 +0200

    Effettuate modifiche a varie classi

--- app/src/main/java/it/unicam/ids/dharma/app/ICliente.java
+++ app/src/main/java/it/unicam/ids/dharma/app/ICliente.java
@@ -9,27 +9,13 @@ public interface ICliente {
     public void cercaProgrammaFedelta();
     public void apriMenuRicerca();
     public void cercaPerAzienda(String nomeAzienda);
-    public void selezionaAzienda(String nomeAzienda);
     public void selezionaProgramma (int idProgramma);
     public void cercaPerTipologia(String tipologia);
     public void selezionaTipologiaProgramma(String tipologia);
-    public void mostraElencoAziende();
     public void mostraElencoProgrammiAzienda (String nomeAzienda);
     public void mostraDettagliProgramma (int idProgramma);
     public void mostraElencoTipologie();
-    public void mostraElencoProgrammiTipologia (String tipologia);
-    public void iniziaRegistrazione();
-    public void accettaContratto();
-    public void inserisciDati(String nome, String cognome, String username, String password);
-    public void richiediDatiCliente();
-    public void inviaDatiCliente(String nome, String cognome, String username, String password);
-    public boolean confermaRegistrazione();
-    public boolean annullaRegistrazione();
-    public void iniziaDisiscrizione();
-    public boolean confermaVolontaDisiscrizionePiattaforma();
-    public void visualizzaMessaggioAllerta();
-    public void chiediConfermaDisiscrizioneProgramma();
-    public void confermaRichiestaDisiscrizioneProgramma();
+    public void mostraElencoProgrammiPerTipologia(String tipologia);
     public void acquistaProdotto(Prodotto prodotto);
     public void cronologiAcquisti();
 }
