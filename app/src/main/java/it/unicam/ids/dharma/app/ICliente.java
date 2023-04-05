fe2fb60a45affd592728e2e148f5ea87c7f045c3
Tania <tania.cartechini@studenti.unicam.it>
2023-04-05 20:23:53 +0200

    Aggiornate classi Acquisto e ICliente

--- app/src/main/java/it/unicam/ids/dharma/app/ICliente.java
+++ app/src/main/java/it/unicam/ids/dharma/app/ICliente.java
@@ -1,5 +1,7 @@
 package it.unicam.ids.dharma.app;
 
+import java.util.Optional;
+
 public interface ICliente {
     /**
      * Il metodo si occupa di cercare un programma fedeltà tra tutti quelli attivabili.
@@ -9,14 +11,13 @@ public interface ICliente {
     public void cercaProgrammaFedelta();
     public void apriMenuRicerca();
     public void cercaPerAzienda(String nomeAzienda);
-    public void selezionaProgramma (int idProgramma);
+    public Optional<ProgrammaFedelta> selezionaProgramma (int idProgramma);
     public void cercaPerTipologia(String tipologia);
     public void selezionaTipologiaProgramma(String tipologia);
     public void mostraElencoProgrammiAzienda (String nomeAzienda);
     public void mostraDettagliProgramma (int idProgramma);
-    public void mostraElencoTipologie();
     public void mostraElencoProgrammiPerTipologia(String tipologia);
-    public void acquistaProdotto(Prodotto prodotto);
+    public void effettuaAcquisto(Prodotto prodotto);
     public void cronologiAcquisti();
     public void visualizzaCatalogoPremi(int idProgramma);
     public void riscattaVantaggio(ProgrammaPunti p, int punti);
