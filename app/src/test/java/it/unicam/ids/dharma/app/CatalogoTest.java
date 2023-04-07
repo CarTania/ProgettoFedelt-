22b936c75dc133a4029bfc17719b075b8814892c
Luca Malvatani <luca.malvatani@studenti.unicam.it>
2023-04-07 11:32:14 +0200

    aggiornata classe CatalogoTest

--- app/src/test/java/it/unicam/ids/dharma/app/CatalogoTest.java
+++ app/src/test/java/it/unicam/ids/dharma/app/CatalogoTest.java
@@ -1,71 +1,107 @@
 package it.unicam.ids.dharma.app;
 
-import org.junit.jupiter.api.Test;
+import it.unicam.ids.dharma.app.Catalogo;
+import it.unicam.ids.dharma.app.Magazzino;
+import it.unicam.ids.dharma.app.Premio;
+import it.unicam.ids.dharma.app.Prodotto;
+import org.junit.jupiter.api.*;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CatalogoTest {
-
+    @BeforeAll
+    static void inizializzaTest(){
+        Magazzino m = Magazzino.getMagazzino();
+        m.svuota();
+    }
     @Test
+    @Order(1)
     public void inserisciPremioTest (){
-        Catalogo catalogo = new Catalogo(1);
-        Prodotto prodotto= new Prodotto("scarpe", 1, 30,4);
-        catalogo.inserisciPremio(prodotto, 4, 10);
-        assertTrue(catalogo.getListapremi().containsKey(prodotto));
-        assertFalse(catalogo.inserisciPremio(prodotto, 4, 10));
-        Prodotto prodotto2= new Prodotto("cinta", 1, 15, 3);
-        assertFalse(catalogo.inserisciPremio(prodotto2, 4, 15));
+        Magazzino m = Magazzino.getMagazzino();
+        Catalogo c = new Catalogo(1);
+        Prodotto prodotto1 = m.aggiungiProdotto("scarpe", 30, 4);
+        assertEquals(4, m.getProdottiDisponibili().get(prodotto1));
+        Premio premio = new Premio(prodotto1, 4, 10);
+        c.inserisciPremio(premio);
+        assertTrue(c.getListapremi().contains(premio));
+        assertFalse(c.inserisciPremio(premio));
+        Prodotto prodotto2 = m.aggiungiProdotto("cinta", 15, 3);
+        assertThrows(
+            IllegalArgumentException.class, () -> {
+                new Premio(prodotto2, 4, 20);
+            });
     }
 
     @Test
+    @Order(2)
     public void rimuoviPremioTest()
     {
-        Catalogo catalogo = new Catalogo(1);
-        Prodotto prodotto= new Prodotto("scarpe", 1, 30,4);
-        catalogo.inserisciPremio(prodotto, 4, 10);
-        assertTrue(catalogo.rimuoviPremio(prodotto));
-        assertFalse(catalogo.rimuoviPremio(prodotto));
+        Magazzino m = Magazzino.getMagazzino();
+        Catalogo c = new Catalogo(1);
+        Prodotto prodotto3 = m.aggiungiProdotto("zaino", 30, 4);
+        Premio premio = new Premio(prodotto3, 4, 10);
+        c.inserisciPremio(premio);
+        assertTrue(c.rimuoviPremio(premio));
+        assertFalse(c.rimuoviPremio(premio));
     }
 
     @Test
+    @Order(3)
     public void aggiornaPuntiPremioTest()
     {
-        Catalogo catalogo = new Catalogo(1);
-        Prodotto prodotto= new Prodotto("scarpe", 1, 30,4);
-        catalogo.inserisciPremio(prodotto, 4, 10);
-        assertTrue(catalogo.aggiornaPuntiPremio(prodotto, 20));
-        assertEquals(catalogo.getListapremi().get(prodotto).getPuntiPremio(), 20);
+        Magazzino m = Magazzino.getMagazzino();
+        Catalogo c = new Catalogo(1);
+        Prodotto prodotto4 = m.aggiungiProdotto("orologio", 30, 4);
+        Premio premio = new Premio(prodotto4, 4, 10);
+        c.inserisciPremio(premio);
+        assertTrue(c.aggiornaPuntiPremio(premio, 20));
+        int indicePremio = c.getListapremi().indexOf(premio);
+        assertEquals(c.getListapremi().get(indicePremio).getPuntiPremio(), 20);
     }
 
     @Test
-    public void aggiornaQuantitaPremioTest()
-    {
-        Catalogo catalogo = new Catalogo(1);
-        Prodotto prodotto= new Prodotto("scarpe", 1, 30,4);
-        catalogo.inserisciPremio(prodotto, 4, 10);
-        assertFalse(catalogo.aggiornaQuantitaPremio(prodotto, 6));
-        Prodotto prodotto2= new Prodotto("gioiello", 1, 300, 20);
-        catalogo.inserisciPremio(prodotto2, 10, 40);
-        assertTrue(catalogo.aggiornaQuantitaPremio(prodotto2, 15));
-        assertEquals(catalogo.getListapremi().get(prodotto2).getQuantitaPremio(), 15);
-        assertEquals(prodotto2.getQuantita(), 5);
+    @Order(4)
+    public void aggiornaQuantitaPremioTest() {
+        Magazzino m = Magazzino.getMagazzino();
+        Catalogo c = new Catalogo(1);
+        Prodotto prodotto5 = m.aggiungiProdotto("cappotto", 30, 4);
+        Premio premio = new Premio(prodotto5, 4, 10);
+        c.inserisciPremio(premio);
+
+        assertFalse(c.aggiornaQuantitaPremio(premio, 6));
+
+        Prodotto prodotto6 = m.aggiungiProdotto("anello", 300, 20);
+        Premio premio2 = new Premio(prodotto6, 10, 40);
+        c.inserisciPremio(premio2);
+
+        assertTrue(c.aggiornaQuantitaPremio(premio2, 15));
+
+        int indicePremio = c.getListapremi().indexOf(premio2);
+
+        assertEquals(c.getListapremi().get(indicePremio).getQuantitaPremio(), 15);
+        assertEquals(Magazzino.getMagazzino().getProdottiDisponibili().get(prodotto6), 5);
     }
 
     @Test
+    @Order(5)
     public void emettiPremioTest() throws CloneNotSupportedException {
-        Catalogo catalogo = new Catalogo(1);
-        Prodotto prodotto= new Prodotto("scarpe", 1, 30,4);
-        catalogo.inserisciPremio(prodotto, 4, 10);
-        Optional<Prodotto> premio= catalogo.emettiPremio(prodotto);
-        assertTrue(premio.isPresent());
-        assertEquals(premio.get().getQuantita(), 1);
-        assertEquals(catalogo.getListapremi().get(prodotto).getQuantitaPremio(), 3);
-        Prodotto prodotto2= new Prodotto("libro", 1, 15, 10);
-        catalogo.inserisciPremio(prodotto2, 0, 9);
-        assertFalse(catalogo.emettiPremio(prodotto2).isPresent());
+        Magazzino m = Magazzino.getMagazzino();
+        Catalogo c = new Catalogo(1);
+        Prodotto prodotto7 = m.aggiungiProdotto("scarpe", 30, 4);
+        Premio premio = new Premio(prodotto7, 4, 10);
+        c.inserisciPremio(premio);
+        Optional<Prodotto> premioAcquisito = c.emettiPremio(premio);
+        assertTrue(premioAcquisito.isPresent());
 
+        int indicePremio = c.getListapremi().indexOf(premio);
+
+        assertEquals(c.getListapremi().get(indicePremio).getQuantitaPremio(), 3);
+
+        Prodotto prodotto8 = m.aggiungiProdotto("libro", 10, 10);
+        Premio premio2 = new Premio(prodotto8, 0, 10);
+        c.inserisciPremio(premio2);
+        assertFalse(c.emettiPremio(premio2).isPresent());
     }
-
 }
